15d10131ea1b5c87a59dd8ebfa90713439c6d6f8
adharuqudni <annas.adharuqudni@tiket.com>
2023-01-03 11:05:00 +0700

    add checking directory

--- index.js
+++ index.js
@@ -3,26 +3,37 @@ const scraperController = require('./controller');
 const express = require('express');
 const puppeteer = require('puppeteer-extra')
 const {executablePath} = require('puppeteer')
-const proxyChain = require('proxy-chain');
+const path = require('path');
+const fs = require('fs');
 
 const app = express();
 const port = 8080;
 
-const oldProxyUrl = 'http://51.159.28.133:8000';
 
 
 // add stealth plugin and use defaults (all evasion techniques)
 const StealthPlugin = require('puppeteer-extra-plugin-stealth')
 puppeteer.use(StealthPlugin())
 
+if (fs.existsSync(path.join(__dirname, 'dump'))) {
+    console.log('Directory exists!')
+  } else {
+    console.log('Directory not found.')
+    fs.mkdir(path.join(__dirname, 'dump'), (err) => {
+        if (err) {
+            return console.error(err);
+        }
+        console.log('Directory created successfully!');
+    });
+  }
+
+
 app.get('/json', async (req, response) => {
     // Web Scraping Code here
     try {
         const {city, date} = req.query;
         //Start the browser and create a browser instance
-        // Pass the browser instance to the scraper controller
-        const newProxyUrl = await proxyChain.anonymizeProxy(oldProxyUrl);
-        
+        // Pass the browser instance to the scraper controller        
         let browserInstance = await puppeteer.launch({
 			headless: true,
 			args: [
