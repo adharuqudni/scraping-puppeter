3dc7599575ac55d57d4a42a344dc933d33845607
adharuqudni <annas.adharuqudni@tiket.com>
2023-01-09 21:44:25 +0700

    Add revision 1 time

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ if (fs.existsSync(path.join(__dirname, 'dump'))) {
 
 const withBrowser = async (fn) => {
     const browser = await puppeteer.launch({
-        headless: true,
+        headless: false,
         args: [
             '--disable-gpu',
             '--disable-setuid-sandbox',
@@ -62,14 +62,44 @@ const withPage = (browser) => async (fn) => {
     }
 }
 
+const scraps = async (params, concurrency) => {
+    const runTime = []
+    return [await withBrowser(async (browser) => {
+        return bluebird.map(params, async (param) => {
+            return withPage(browser)(async (page) => {
+                const beforeRun = moment();
+                await page.setViewport({
+                    width: 1920,
+                    height: 3000,
+                    deviceScaleFactor: 1,
+                    hasTouch: false,
+                    isLandscape: false,
+                    isMobile: false,
+                });
+                const userAgent = randomUseragent.getRandom();
+                const UA = userAgent || USER_AGENT;
+                await page.setUserAgent(UA);
+                await page.setJavaScriptEnabled(true);
+                await page.setDefaultNavigationTimeout(0);
+                await page.goto('https://m.tiket.com/sewa-mobil');
+                console.log(`Navigating to https://m.tiket.com/sewa-mobil...`);
+                const data = await scraperController(page, param.city, param.date)
+                const afterRun = moment().diff(beforeRun, 'minutes', true);
+                runTime.push(afterRun);
+                return data
+            });
+        }, { concurrency: parseInt(concurrency) || 1 });
+    }), runTime];
+}
+
 
 app.get('/json', async (req, response) => {
     // Web Scraping Code here
     console.log(req.query)
 
     const { citys, concurrency } = req.query;
-    const runTime = []
     const params = []
+    let revisionData, revisionRuntime = []
     if(!citys) {
         return response.status(500).json({
             message: 'Missing Params citys',
@@ -88,39 +118,30 @@ app.get('/json', async (req, response) => {
             })
         })
     })
+    
 
     try {
-        const results = await withBrowser(async (browser) => {
-            return bluebird.map(params, async (param) => {
-                return withPage(browser)(async (page) => {
-                    const beforeRun = moment();
-                    await page.setViewport({
-                        width: 1920,
-                        height: 3000,
-                        deviceScaleFactor: 1,
-                        hasTouch: false,
-                        isLandscape: false,
-                        isMobile: false,
-                    });
-                    const userAgent = randomUseragent.getRandom();
-                    const UA = userAgent || USER_AGENT;
-                    await page.setUserAgent(UA);
-                    await page.setJavaScriptEnabled(true);
-                    await page.setDefaultNavigationTimeout(0);
-                    await page.goto('https://m.tiket.com/sewa-mobil');
-                    console.log(`Navigating to https://m.tiket.com/sewa-mobil...`);
-                    const data = await scraperController(page, param.city, param.date)
-                    const afterRun = moment().diff(beforeRun, 'minutes', true);
-                    runTime.push(afterRun);
-                    return data
-                });
-            }, { concurrency: parseInt(concurrency) || 1 });
-        });
+        const [data, runtime] = await scraps(params, concurrency)
+        const revisionParams = []
+        data.map( (result, index) => {
+            if(!(result?.length > 0)){
+                revisionParams.push(params[index])
+                data.splice(index, 1)
+                runtime.splice(index, 1)
+            }
+        })
+        if(revisionParams?.length > 0) {
+            console.log('kena revisi')
+            [revisionData, revisionRuntime] = await scraps(revisionParams, concurrency)
+        }
+        revisionRuntime = [...revisionRuntime, ...runtime]
+        revisionData = [...revisionData, ...data]
+
 
         response.status(200).json({ 
-            total: results.map( (result,index) => ({  [`Total-${params[index].city}-next(${params[index].date})days`] : result?.length || 'Error'})), 
-            runtime: runTime.map( (run,index) => ({  [`Runtime-${params[index].city}-next(${params[index].date})days`] : `${run} minutes`})), 
-            results 
+            total: revisionData.map( (result,index) => ({  [`Total-${params[index].city}-next(${params[index].date})days`] : result?.length || 'Error'})), 
+            runtime: revisionRuntime.map( (run,index) => ({  [`Runtime-${params[index].city}-next(${params[index].date})days`] : `${run} minutes`})), 
+            revisionData 
         });
     } catch (error) {
         console.log(error);
